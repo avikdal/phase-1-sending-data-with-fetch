e206f7b1387e631895daaba2aa0986a923aad150
amanda vikdal <vikdal.amanda@gmail.com>
2022-12-09 11:14:31 -0700

    commit tree 028ec1b7917ec3803cb1cdb633b727b9029cb341

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// function takes two strings as arguments
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        }) 
+    })
+    .then((res) => res.json())
+    .then((obj) => document.querySelector('body').append(obj.id))
+    .catch((error) => { alert("whoopsie, try again"); 
+    document.querySelector('body').append(error.message);
+})
+}
+
+submitData("jo", "hotmail");
